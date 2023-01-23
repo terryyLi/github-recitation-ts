d4b3b37b6f5f620c752843fe8b9afd0835f334a4
terryyLi <jielinl@andrew.cmu.edu>
2023-01-23 10:21:29 -0500

    fixed bugs in fibRoute.ts

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,12 +1,12 @@
 // Endpoint for querying the fibonacci numbers
 
-const fibonacci = require("./fib");
+import fibonacci from "./fib";
 
 export default (req, res) => {
   const { num } = req.params;
 
-  const fibN = fibonacci(parseInt(num));
-  let result = `fibonacci(${num}) is ${fibN}`;
+  const fibN: number = fibonacci(parseInt(num));
+  let result: string = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
     result = `fibonacci(${num}) is undefined`;
